9dfa788fefbc5e179a8deb3ba0ee10c72392c7ef
GrantSparkVibe <grantsparkvibe@gmail.com>
2025-06-24 22:57:44 +0300

    Add working React app in frontend

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Grant Spark Vibe</h1>
+        <p>
+          Welcome to Grant Spark Vibe - Your platform for discovering grant opportunities.
+        </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
